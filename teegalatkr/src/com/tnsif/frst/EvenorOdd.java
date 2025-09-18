b6aa651e23a419c798ae2a198fd14a1887650b04
piska <piska@Nani>
2025-09-18 20:36:48 +0530

    This is first commit

---
+++ teegalatkr/src/com/tnsif/frst/EvenorOdd.java
@@ -0,0 +1,11 @@
+package com.tnsif.frst;
+
+class EvenOdd {
+	  public static void main(String[] args) {
+	    int n = 5;   // you can change the number
+	    if(n % 2 == 0)
+	      System.out.println(n + " is Even");
+	    else
+	      System.out.println(n + " is Odd");
+	  }
+	}
